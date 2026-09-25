816a99af876f96eb93cba9b676fc6ec709e2bd82
BraydenBride <bride62097@students.apsb.org>
2026-09-25 12:45:32 +0000

    message

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -21,10 +21,17 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 
+        var circles = [];
+
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            var circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+         }
 
 
         // TODO 3 : Call the drawCircle() function
@@ -32,6 +39,10 @@ var init = function (window) {
 
 
         // TODO 7 : Use a loop to create multiple circles
+        for (var i = 0; i < 200; i++) {
+            drawCircle(200);
+}
+
 
 
 
@@ -50,13 +61,13 @@ var init = function (window) {
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+            for (var i = 0; i < circles.length; i++) {
+                 physikz.updatePosition(circles[i]);
+                 game.checkCirclePosition(circles[i]);
+            }
         }
-    
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -70,12 +81,17 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.x + circle.radius < 0){
+                circle.x = canvas.width
+            }
+            if (circle.y + circle.radius < 0){
+                circle.y = canvas.height
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
-        
+    
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
         /////////////////////////////////////////////////////////////
